60c47df5f5fbe04a02dfe735a7e416b91992590b
bejmach <bejmach@gmail.com>
2024-09-25 17:38:00 +0200

    YOU fucking asynchronous piece of shit Who allowed you to do res.end, which was after all if's, before them. Why this language is so fucked up. Async build in??? Dont make me laught. why this works like that. That should be illegal

--- index.js
+++ index.js
@@ -10,23 +10,24 @@ http.createServer(function (req, res) {
 	console.log(q.pathname);
 	if(q.pathname == '/'){
 		console.log('/');
-		res.write('Strona Glowna');
+		res.end('Strona Glowna');
 	}
 	else if(q.pathname == '/json'){
 		console.log('/json');
-		res.write('{"json":{"servers":{"users"{"user"{"name":"test","id":1}}}}}');
+		res.writeHead(200, {'Content-Type': 'application/json'});
+		res.end('{"json":{"servers":{"users"{"user"{"name":"test","id":1}}}}}');
 	}
 	else if(q.pathname == '/htmlGen'){
 		console.log('/htmlGen');
 		var html = '<H1>Test page</H1><div>node js from code</div>'
-		res.write(html);
+		res.end(html);
 	}
 	else if(q.pathname == '/htmlFile'){
 
 		console.log('/htmlFile');
 		fs.readFile('index.html', function(err, data) {
 			res.writeHead(200, {'Content-Type': 'text/html'});
-			res.write(data);
+			res.end(data);
 		})
 	}
 	else if(q.pathname == '/get_params'){
@@ -41,32 +42,26 @@ http.createServer(function (req, res) {
 				console.error(err);
 			}
 		});
-		res.write('{"ok": "ok"}');
+		res.end('{"ok": "ok"}');
 	}
 	else{
 		console.log('assets'+q.pathname);
-		console.log(mime.lookup('assets'+q.pathname));
 		fs.readFile('./assets'+q.pathname, 'utf8', function(err, data){
 			if(err){
-				console.log('error');
+				res.writeHead(200, {'Content-Type': 'application/json'});
 				res.statusCode = 404;
-				res.write(JSON.stringify({error: 404}));
+				res.end(JSON.stringify({error: 404}));
 			}
 			else{
 				var mimeVar = mime.lookup('./assets'+q.pathname);
-				//res.writeHead('Content-Type', mimeVar);
-				console.log('head');
+				res.writeHead(200, {'Content-Type': mimeVar});
 				if(mimeVar == 'application/json'){
-					res.write(JSON.stringify(data));
+					res.end(JSON.stringify(data));
 				}
 				else{
 					res.end(data);
-					console.log(data);
 				}
-				res.write(data+'2');
 			}
-			res.write(data+'3');
 		})
 	}
-	res.end();
 }).listen(port);
