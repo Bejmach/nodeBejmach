8e7f88f2a12dd67b73ebbf9e9ba33c361db0cf61
Bejmach <bejmach@gmail.com>
2024-12-02 07:53:38 +0100

    crudStart

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
-var express = require('express');
-var prisma = require('prisma');
-var router = express.Router();
+const express = require('express');
+const prisma = require('prisma');
+const PrismaClient = require("@prisma/client");
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/api/comment/:userName/:text', function(req, res, next){
+  const comment = prisma.Komentarz.create({
+      data:{
+        user_name:req.user_name,
+        comment:req.text,
+      }
+
+  })
+})
 
 module.exports = router;
