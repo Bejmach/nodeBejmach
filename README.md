74d62e7100aeea692671329e57ea7ea9955bf59d
Bejmach <bejmach@gmail.com>
2024-12-21 10:49:16 +0100

    crud operations(not tested)

--- README.md
+++ README.md
@@ -4,3 +4,15 @@
 * create empty database
 * create .env file in root folder with DATABASE_URL set to url of the database
 * run "npx prisma migrate dev --name init"
+
+###create
+/api/comment/create/:userName/:text
+
+###update
+/api/comment/update/:id/:userName/:text
+
+###remove
+/api/comment/delete/:id
+
+###find
+/api/comment/find/:id
--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/api/comment/:userName/:text', function(req, res, next){
+router.get('/api/comment/create/:userName/:text', function(req, res, next){
   const comment = prisma.Komentarz.create({
       data:{
         user_name:req.user_name,
@@ -16,6 +16,37 @@ router.get('/api/comment/:userName/:text', function(req, res, next){
       }
 
   })
-})
+});
+
+router.get('/api/comment/find/:id', function(req, res, next){
+  const comment = prisma.Komentarz.findUnique({
+      where:{
+        id: req.id,
+      }
+  })
+  console.log(comment);
+});
+
+router.get('/api/comment/delete/:id', function(req, res, next){
+  const comment = prisma.Komentarz.create({
+      where: {
+        id: req.id,
+      }
+  })
+});
+
+router.get('/api/comment/update/:id/:userName/:text', function(req, res, next){
+  const comment = prisma.Komentarz.create({
+      where: {
+        if: req.id,
+      },
+      data:{
+        user_name:req.user_name,
+        comment:req.text,
+      }
+
+  })
+});
+
 
 module.exports = router;
